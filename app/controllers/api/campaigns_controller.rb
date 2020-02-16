ff4b0b48a2e3e9031b681aeb01ce66d1785f3ee0
ToriK17 <vfcad@protonmail.com>
2020-02-16 13:03:25 -0800

    adds campaign controller and views

--- app/controllers/api/campaigns_controller.rb
+++ app/controllers/api/campaigns_controller.rb
@@ -1,2 +1,33 @@
 class Api::CampaignsController < ApplicationController
+  def index
+    @campaigns = Campaign.all
+    render "index.json.jb"
+  end
+
+  def create
+    @campaign = Campaign.new(
+      title: params[:title], 
+      details: params[:details])
+    @campaign.save
+    render "show.json.jb"
+  end
+
+  def show
+    @campaign = Campaign.find(params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @campaign = Campaign.find(params[:id])
+    @campaign.title = params[:title] || @campaign.title 
+    @campaign.details = params[:details] || @campaign.details 
+    @campaign.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    campaign = Campaign.find(params[:id])
+    campaign.destroy
+    render json: {message: "campaign destroyed."}
+  end
 end
