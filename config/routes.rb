2333922842156a9745bcc4dcede915cfb50cb9a4
ToriK17 <vfcad@protonmail.com>
2020-02-16 12:30:22 -0800

    adds restful routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,25 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/campaigns" => "campaigns#index"
+    post "/campaigns" => "campaigns#create"
+    get "/campaigns/:id" => "campaigns#show"
+    patch "/campaigns/:id" => "campaigns#update"
+    delete "/campaigns/:id" => "campaigns#destroy"
+
+    get "/characters" => "characters#index"
+    post "/characters" => "characters#create"
+    get "/characters/:id" => "characters#show"
+    patch "/characters/:id" => "characters#update"
+    delete "/characters/:id" => "characters#destroy"
+
+    get "/users" => "users#index"
+    post "/users" => "users#create"
+    get "/users/:id" => "users#show"
+    patch "/users/:id" => "users#update"
+    delete "/users/:id" => "users#destroy"
+
+  end
+
 end
